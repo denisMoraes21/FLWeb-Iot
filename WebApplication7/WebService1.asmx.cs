50483d0548ef4995b2f12626a99ffbcd5b1c144d
Denis Moraes <denisguimaraes2012@gmail.com>
2021-05-09 19:07:05 -0400

    Adicionar arquivos de projeto.

---
+++ WebApplication7/WebService1.asmx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.IO;
+
+namespace WebApplication7
+{
+    /// <summary>
+    /// Webservice REST para upload de arquivos
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    
+    public class WebService1 : System.Web.Services.WebService
+    {
+        public void main(string[] args)
+        {
+        }
+        [WebMethod]
+        public string Upload(String Arquivo1, byte[] tamanho)
+        {
+            string arquivoPC = @"C:\Users\denis\Documents\1.txt";
+            if (File.Exists(arquivoPC))
+            {
+                try
+                {
+                    String arquivoWeb = Server.MapPath(arquivoPC) + Arquivo1;
+                    File.WriteAllBytes(arquivoWeb, tamanho);
+                }
+                catch
+                {
+                    return "Erro de upload!";
+                }
+            }
+            else
+            {
+                return "Arquivo inexistente!";
+            }
+
+                return "Upload feito!";
+        }
+    }
+}
